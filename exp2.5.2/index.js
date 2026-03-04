28f2b6bdd534a0cc75fd4ab557205fb9d1e7b220
Aditya Pathania <adipathania2006@gmail.com>
2026-03-05 02:15:54 +0530

    exp4

--- exp2.5.2/index.js
+++ exp2.5.2/index.js
@@ -71,7 +71,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
   console.log(`Student Management System Started`);
