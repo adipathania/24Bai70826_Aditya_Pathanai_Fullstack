cd32223109d9bd82c6a0f071d1ee76b6e75ab286
Aditya Pathania <adipathania2006@gmail.com>
2026-04-15 01:40:51 +0530

    exp9

---
+++ exp9-main/exp9.1/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{textAlign: "center", marginTop: "50px"}}>
+      <h1>🚀 Experiment 9.1 - Dockerized React App</h1>
+      <p>Created by SCOUT1406</p>
+      <p>Email: 24BAI70817@cuchd.in</p>
+    </div>
+  );
+}
+
+export default App;
